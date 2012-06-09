69145c1b908aae2ef1be52cbe703ad0ed341ed2b
Hector Castro <hectcastro@gmail.com>
2012-06-09 14:06:37 -0400

    Added Graphite 0.9.10 support.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default["graphite"]["version"]                              = "0.9.9"
+default["graphite"]["version"]                              = "0.9.10"
 default["graphite"]["carbon"]["line_receiver_interface"]    = "127.0.0.1"
 default["graphite"]["carbon"]["pickle_receiver_interface"]  = "127.0.0.1"
 default["graphite"]["carbon"]["cache_query_interface"]      = "127.0.0.1"
