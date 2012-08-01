a07140e220e2e038792136d53b1521e6b763a7f9
Lew Goettner <lew@goettner.net>
2012-08-01 14:14:11 -0400

    Add default template to attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,12 +4,14 @@ default["graphite"]["carbon"]["pickle_receiver_interface"]  = "127.0.0.1"
 default["graphite"]["carbon"]["cache_query_interface"]      = "127.0.0.1"
 default["graphite"]["dashboard"]["timezone"]                = "America/New_York"
 default["graphite"]["dashboard"]["memcache_hosts"]          = [ "127.0.0.1:11211" ]
-# default["graphite"]["templates"]["default"]["background"]   = 
-# default["graphite"]["templates"]["default"]["foreground"]   = 
-# default["graphite"]["templates"]["default"]["majorLine"]    = 
-# default["graphite"]["templates"]["default"]["minorLine"]    = 
-# default["graphite"]["templates"]["default"]["lineColors"]   = 
-# default["graphite"]["templates"]["default"]["fontName"]     = 
-# default["graphite"]["templates"]["default"]["fontSize"]     = 
-# default["graphite"]["templates"]["default"]["fontBold"]     = 
-# default["graphite"]["templates"]["default"]["fontItalic"]   = 
+
+# The default values
+default["graphite"]["templates"]["default"]["background"]   = "black"
+default["graphite"]["templates"]["default"]["foreground"]   = "white"
+default["graphite"]["templates"]["default"]["majorLine"]    = "white"
+default["graphite"]["templates"]["default"]["minorLine"]    = "grey"
+default["graphite"]["templates"]["default"]["lineColors"]   = "blue,green,red,purple,brown,yellow,aqua,grey,magenta,pink,gold,rose"
+default["graphite"]["templates"]["default"]["fontName"]     = "Sans"
+default["graphite"]["templates"]["default"]["fontSize"]     = "10"
+default["graphite"]["templates"]["default"]["fontBold"]     = "False"
+default["graphite"]["templates"]["default"]["fontItalic"]   = "False"
